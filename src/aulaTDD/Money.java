6477452ad6933106bdf90f701d9b9ce596085b16
Jvccampos <jok772010@gmail.com>
2019-11-04 04:02:23 -0300

    Commit 4

--- src/aulaTDD/Money.java
+++ src/aulaTDD/Money.java
@@ -2,16 +2,21 @@ package aulaTDD;
 
 abstract class Money  {
    protected int amount;
-   Money(int amount) {
+   protected String currency;
+   Money(int amount, String currency) {
        this.amount= amount;
+       this.currency = currency;
+       
     }
-   
+   String currency() {
+      return currency;
+   }
    static Money dollar(int amount)  {
-      return new Dollar(amount);
+      return new Dollar(amount, "USD");
    }
    
    static Money franc(int amount) {
-      return new Franc(amount);
+      return new Franc(amount, "CHF");
     }
    abstract Money times(int multiplier);
    public boolean equals(Object object) {
@@ -20,3 +25,20 @@ abstract class Money  {
        return (this.amount == money.amount) && getClass().equals(money.getClass());
    }
 }
+class Franc extends Money{   		
+   Franc(int amount, String currency) {      
+      super(amount, currency);
+   }
+    Money times(int multiplier)  {      
+       return Money.franc(amount * multiplier);					
+    }   				
+}
+class Dollar extends Money{
+    Dollar(int amount, String currency) {
+        super(amount, currency);
+     }
+     Money times(int multiplier) {
+        return Money.dollar(amount * multiplier);
+     }
+}
+
--- src/aulaTDD/MoneyTest.java
+++ src/aulaTDD/MoneyTest.java
@@ -6,12 +6,9 @@ import org.junit.jupiter.api.Test;
 
 class MoneyTest {
 	@Test
-	public void testEquality() {
-	   assertTrue(new Dollar(5).equals(new Dollar(5)));
-	   assertFalse(new Dollar(5).equals(new Dollar(6)));
-	   assertTrue(new Franc(5).equals(new Franc(5)));
-	   assertFalse(new Franc(5).equals(new Franc(6)));
-	   assertFalse(new Franc(5).equals(new Dollar(5)));
+	public void testCurrency() {
+	   assertEquals("USD", Money.dollar(1).currency());
+	   assertEquals("CHF", Money.franc(1).currency());
 	}
 	@Test
 	public void testMultiplication() {
